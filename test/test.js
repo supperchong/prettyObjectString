172e0bd058eb14d74f7152cb7f0aed58a9b0413e
supperchong <supperchong@gmail.com>
2019-04-03 10:25:06 +0800

    add test example

---
+++ test/test.js
@@ -0,0 +1,16 @@
+const JsToJson=require('../lib/index')
+const assert = require('chai').assert
+
+
+describe('pretty js string',()=>{
+    it('pretty object',()=>{
+        const str='{name:\'age\'}'       
+        let out=JsToJson(str)
+        assert.equal(out,'{\n\t"name": "age"\n}')
+    })
+    it('pretty array',()=>{
+        const str='[{name:\'age\'},null,"people",321,true,false]'       
+        let out=JsToJson(str)
+        assert.equal(out,'[\n\t{\n\t\t"name": "age"\n\t},\n\tnull,\n\t"people",\n\t321,\n\ttrue,\n\tfalse\n]')
+    })
+})
