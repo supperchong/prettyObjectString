d6248f2a40ba1b07de946df64d5569074fe8c3b3
supperchong <supperchong@gmail.com>
2020-07-04 14:59:21 +0800

    chore(type): add  partialIndex options

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,9 @@
 export = JsToJson;
-declare function JsToJson(src: string,options?:JsToJson.Options): string;
+declare function JsToJson(src: string, options?: JsToJson.Options): string;
 declare namespace JsToJson {
     export interface Options {
         delComment?: boolean;
         indent?: string;
+        partialIndex?: boolean;
     }
 }
