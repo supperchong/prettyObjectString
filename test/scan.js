a001c1ba4cb960346d6dfbdc489442f9c323071f
supperchong <supperchong@gmail.com>
2020-07-04 10:54:47 +0800

    feat(core): support getting index of  valid javascript value from start

--- test/scan.js
+++ test/scan.js
@@ -3,200 +3,197 @@ let Scan = require('../lib/scan')
 const MyError = require('../lib/myError')
 const { CommonCodes, getMsg } = require('../lib/errorCode')
 const { Kind, ParseState, ObjectKeyState } = require('../lib/constants')
+
 const valueExamples = [
-    ['string', '"name\\u1111"'],
-    ['number', '112'],
-    ['object', '{"name":"li",\'age\':\'21\\\'\\u1234\'}'],
-    ['array', '["name",null]'],
-    ['true', 'true'],
-    ['false', 'false'],
-    ['null', 'null'],
-    ['array', '[-0.34e-34,-12.34e+34,-0e+3,-0e9,-1]'],
+  ['string', '"name\\u1111"'],
+  ['number', '112'],
+  ['object', "{\"name\":\"li\",'age':'21\\'\\u1234'}"],
+  ['array', '["name",null]'],
+  ['true', 'true'],
+  ['false', 'false'],
+  ['null', 'null'],
+  ['array', '[-0.34e-34,-12.34e+34,-0e+3,-0e9,-1]'],
 ]
 const Errors = [
-    ['"name",', CommonCodes.EndError],
-    ['{name:age}', CommonCodes.InvalidValue],
-    [
-        `{
+  ['"name",', CommonCodes.EndError],
+  ['{name:age}', CommonCodes.InvalidValue],
+  [
+    `{
         name:1,
         age:1,
     }`,
-        CommonCodes.InvalidValue,
-    ],
-    ['"name"{', CommonCodes.EndError],
-    ['nl', CommonCodes.InvalidValue],
-    ['nua', CommonCodes.InvalidValue],
-    ['nula', CommonCodes.InvalidValue],
-    ['ff', CommonCodes.InvalidValue],
-    ['faf', CommonCodes.InvalidValue],
-    ['falf', CommonCodes.InvalidValue],
-    ['falsf', CommonCodes.InvalidValue],
-    ['ta', CommonCodes.InvalidValue],
-    ['tra', CommonCodes.InvalidValue],
-    ['trua', CommonCodes.InvalidValue],
-    ['"\\uz"', CommonCodes.InvalidValue],
-    ['"\\u1z"', CommonCodes.InvalidValue],
-    ['"\\u11z"', CommonCodes.InvalidValue],
-    ['"\\u111z"', CommonCodes.InvalidValue],
-    ['"\\z"', CommonCodes.InvalidValue],
-    ['{name:asw', CommonCodes.InvalidValue],
-    ['["name"asw', CommonCodes.InvalidValue],
-    ['{"name"asw', CommonCodes.InvalidValue],
-    ['{"name":"li"asw', CommonCodes.InvalidValue],
-    ['{2name:"li"}', CommonCodes.InvalidObjectKey],
+    CommonCodes.InvalidValue,
+  ],
+  ['"name"{', CommonCodes.EndError],
+  ['nl', CommonCodes.InvalidValue],
+  ['nua', CommonCodes.InvalidValue],
+  ['nula', CommonCodes.InvalidValue],
+  ['ff', CommonCodes.InvalidValue],
+  ['faf', CommonCodes.InvalidValue],
+  ['falf', CommonCodes.InvalidValue],
+  ['falsf', CommonCodes.InvalidValue],
+  ['ta', CommonCodes.InvalidValue],
+  ['tra', CommonCodes.InvalidValue],
+  ['trua', CommonCodes.InvalidValue],
+  ['"\\uz"', CommonCodes.InvalidValue],
+  ['"\\u1z"', CommonCodes.InvalidValue],
+  ['"\\u11z"', CommonCodes.InvalidValue],
+  ['"\\u111z"', CommonCodes.InvalidValue],
+  ['"\\z"', CommonCodes.InvalidValue],
+  ['{name:asw', CommonCodes.InvalidValue],
+  ['["name"asw', CommonCodes.InvalidValue],
+  ['{"name"asw', CommonCodes.InvalidValue],
+  ['{"name":"li"asw', CommonCodes.InvalidValue],
+  ['{2name:"li"}', CommonCodes.InvalidObjectKey],
 ]
 const {
-    scanContinue,
-    scanBeginLiteral,
-    scanBeginObject,
-    scanObjectKey,
-    scanObjectValue,
-    scanEndObject,
-    scanBeginArray,
-    scanArrayValue,
-    scanEndArray,
-    scanSkipSpace,
-    scanSkipObjectComma,
-    scanSingleQuotationKey,
-    scanSingleQuotationKeyEnd,
-    scanDoubleQuotesKey,
-    scanDoubleQuotesKeyEnd,
-    scanSingleQuotationValue,
-    scanSingleQuotationValueEnd,
-    scanDoubleQuotesValue,
-    scanDoubleQuotesValueEnd,
-    scanEmptyKey,
-    scanEmptyKeyEnd,
-    scanSingleCharEmptyKeyEnd,
-    scanEnd,
-    scanError,
+  scanContinue,
+  scanBeginLiteral,
+  scanBeginObject,
+  scanObjectKey,
+  scanObjectValue,
+  scanEndObject,
+  scanBeginArray,
+  scanArrayValue,
+  scanEndArray,
+  scanSkipSpace,
+  scanSkipObjectComma,
+  scanSingleQuotationKey,
+  scanSingleQuotationKeyEnd,
+  scanDoubleQuotesKey,
+  scanDoubleQuotesKeyEnd,
+  scanSingleQuotationValue,
+  scanSingleQuotationValueEnd,
+  scanDoubleQuotesValue,
+  scanDoubleQuotesValueEnd,
+  scanEmptyKey,
+  scanEmptyKeyEnd,
+  scanSingleCharEmptyKeyEnd,
+  scanEnd,
+  scanError,
 } = Kind
-function getCharArrs(str) {
-    let chars = []
-    for (const c of str) {
-        chars.push(c)
-    }
-    return chars
+function getChars(str) {
+  return str
 }
 
 describe('scan json value', () => {
-    // let scan
-    // beforeEach(() => {
-    //     scan = new Scan()
-    //     scan.reset()
-    // })
-    valueExamples.forEach(([type, example]) => {
-        it(`scan ${type}`, () => {
-            let chars = getCharArrs(example)
-            let scan = new Scan(chars)
-            for (let i = 0; i < chars.length; i++) {
-                let c = chars[i]
-                let n = chars[i + 1]
-                scan.step(c, n, i)
-            }
-        })
+  // let scan
+  // beforeEach(() => {
+  //     scan = new Scan()
+  //     scan.reset()
+  // })
+  valueExamples.forEach(([type, example]) => {
+    it(`scan ${type}`, () => {
+      let chars = getChars(example)
+      let scan = new Scan(chars)
+      for (let i = 0; i < chars.length; i++) {
+        let c = chars[i]
+        let n = chars[i + 1]
+        scan.step(c, n, i)
+      }
     })
+  })
 })
 describe('scan json error', () => {
-    // let scan
-    // beforeEach(() => {
-    //     scan = new Scan()
-    //     scan.reset()
-    // })
-    Errors.forEach(([example, error]) => {
-        it(`expect ${error.msg}`, () => {
-            let chars = getCharArrs(example)
-            let scan = new Scan(chars)
-            for (let i = 0; i < chars.length; i++) {
-                let c = chars[i]
-                let n = chars[i + 1]
-                try {
-                    scan.step(c, n, i)
-                } catch (err) {
-                    assert.equal(err.code, error.code)
-                    return
-                }
-            }
-        })
+  // let scan
+  // beforeEach(() => {
+  //     scan = new Scan()
+  //     scan.reset()
+  // })
+  Errors.forEach(([example, error]) => {
+    it(`expect ${error.msg}`, () => {
+      let chars = getChars(example)
+      let scan = new Scan(chars)
+      for (let i = 0; i < chars.length; i++) {
+        let c = chars[i]
+        let n = chars[i + 1]
+        try {
+          scan.step(c, n, i)
+        } catch (err) {
+          assert.equal(err.code, error.code)
+          return
+        }
+      }
     })
+  })
 })
 describe('test scan state', () => {
-    it('scan single quotation string begin', () => {
-        let originStr = '\''
-        let chars = getCharArrs(originStr)
-        let scan = new Scan(chars)
-        let state = scan.getStateAt(0)
-        assert.equal(state, scanSingleQuotationValue)
-    })
-    it('scan single quotation string end', () => {
-        let originStr = '\'name\''
-        let chars = getCharArrs(originStr)
-        let scan = new Scan(chars)
-        let state = scan.getStateAt(originStr.length - 1)
-        assert.equal(state, scanSingleQuotationValueEnd)
-    })
-    it('begin scan object single key begin', () => {
-        let originStr = '{\''
-        let chars = getCharArrs(originStr)
-        let scan = new Scan(chars)
-        let state = scan.getStateAt(originStr.length - 1)
-        assert.equal(state, scanSingleQuotationKey)
-    })
-    it('begin scan object single key end', () => {
-        let originStr = '{\'asd\''
-        let chars = getCharArrs(originStr)
-        let scan = new Scan(chars)
-        let state = scan.getStateAt(originStr.length - 1)
-        assert.equal(state, scanSingleQuotationKeyEnd)
-    })
-    it('begin scan object end', () => {
-        let originStr = `
+  it('scan single quotation string begin', () => {
+    let originStr = "'"
+    let chars = getChars(originStr)
+    let scan = new Scan(chars)
+    let state = scan.getStateAt(0)
+    assert.equal(state, scanSingleQuotationValue)
+  })
+  it('scan single quotation string end', () => {
+    let originStr = "'name'"
+    let chars = getChars(originStr)
+    let scan = new Scan(chars)
+    let state = scan.getStateAt(originStr.length - 1)
+    assert.equal(state, scanSingleQuotationValueEnd)
+  })
+  it('begin scan object single key begin', () => {
+    let originStr = "{'"
+    let chars = getChars(originStr)
+    let scan = new Scan(chars)
+    let state = scan.getStateAt(originStr.length - 1)
+    assert.equal(state, scanSingleQuotationKey)
+  })
+  it('begin scan object single key end', () => {
+    let originStr = "{'asd'"
+    let chars = getChars(originStr)
+    let scan = new Scan(chars)
+    let state = scan.getStateAt(originStr.length - 1)
+    assert.equal(state, scanSingleQuotationKeyEnd)
+  })
+  it('begin scan object end', () => {
+    let originStr = `
         {
             name:1,
             age:1,
         }
         `
-        let chars = getCharArrs(originStr)
-        let scan = new Scan(chars)
-        let state = scan.getStateAt(originStr.length - 1)
-        assert.equal(Kind[state], Kind[scanEnd])
-    })
-    it('begin scan array', () => {
-        let originStr = '['
-        let chars = getCharArrs(originStr)
-        let scan = new Scan(chars)
-        let state = scan.getStateAt(originStr.length - 1)
-        assert.equal(state, scanBeginArray)
-    })
-    it('scan array end', () => {
-        let originStr = '[]'
-        let chars = getCharArrs(originStr)
-        let scan = new Scan(chars)
-        let state = scan.getStateAt(originStr.length - 1)
-        assert.equal(state, scanEndArray)
-    })
-    it('scan compose array with string', () => {
-        let originStr = '[\'name\',"age"]'
-        let chars = getCharArrs(originStr)
-        let scan = new Scan(chars)
-        let state = scan.getStateAt(originStr.length - 1)
-        assert.equal(state, scanEndArray)
-    })
-    it('scan compose array with string', () => {
-        let originStr = '[3,1,\'name\''
-        let chars = getCharArrs(originStr)
-        let scan = new Scan(chars)
-        let state = scan.getStateAt(originStr.length - 1)
-        assert.equal(state, scanSingleQuotationValueEnd)
-    })
+    let chars = getChars(originStr)
+    let scan = new Scan(chars)
+    let state = scan.getStateAt(originStr.length - 1)
+    assert.equal(Kind[state], Kind[scanEnd])
+  })
+  it('begin scan array', () => {
+    let originStr = '['
+    let chars = getChars(originStr)
+    let scan = new Scan(chars)
+    let state = scan.getStateAt(originStr.length - 1)
+    assert.equal(state, scanBeginArray)
+  })
+  it('scan array end', () => {
+    let originStr = '[]'
+    let chars = getChars(originStr)
+    let scan = new Scan(chars)
+    let state = scan.getStateAt(originStr.length - 1)
+    assert.equal(state, scanEndArray)
+  })
+  it('scan compose array with string', () => {
+    let originStr = '[\'name\',"age"]'
+    let chars = getChars(originStr)
+    let scan = new Scan(chars)
+    let state = scan.getStateAt(originStr.length - 1)
+    assert.equal(state, scanEndArray)
+  })
+  it('scan compose array with string', () => {
+    let originStr = "[3,1,'name'"
+    let chars = getChars(originStr)
+    let scan = new Scan(chars)
+    let state = scan.getStateAt(originStr.length - 1)
+    assert.equal(state, scanSingleQuotationValueEnd)
+  })
 })
 
 describe('test indent', () => {
-    it('scan single quotation string begin', () => {
-        let originStr = '\''
-        let chars = getCharArrs(originStr)
-        let scan = new Scan(chars)
-        let state = scan.getStateAt(0)
-        assert.equal(state, scanSingleQuotationValue)
-    })
+  it('scan single quotation string begin', () => {
+    let originStr = "'"
+    let chars = getChars(originStr)
+    let scan = new Scan(chars)
+    let state = scan.getStateAt(0)
+    assert.equal(state, scanSingleQuotationValue)
+  })
 })
--- test/test.js
+++ test/test.js
@@ -2,63 +2,128 @@ const JsToJson = require('../lib/index')
 const assert = require('chai').assert
 
 describe('pretty js string', () => {
-    it('pretty object', () => {
-        const str = '{name:\'li\',age:20}'
-        let out = JsToJson(str)
-        assert.equal(out, '{\n\t"name": "li",\n\t"age": 20\n}')
+  it('pretty object', () => {
+    const str = "{name:'li',age:20}"
+    let out = JsToJson(str)
+    assert.equal(out, '{\n\t"name": "li",\n\t"age": 20\n}')
+  })
+  it('pretty single char empty quotation key object', () => {
+    const str = '{a:1}'
+    let out = JsToJson(str)
+    assert.equal(out, '{\n\t"a": 1\n}')
+  })
+  it('pretty empty  object', () => {
+    const str = '{}'
+    let out = JsToJson(str)
+    assert.equal(out, '{}')
+  })
+  it('pretty  object last comma', () => {
+    const str = '{a:1,}'
+    let out = JsToJson(str)
+    assert.equal(out, '{\n\t"a": 1\n}')
+  })
+  it('pretty single quotation object', () => {
+    const str = "{'name':'li'}"
+    let out = JsToJson(str)
+    assert.equal(out, '{\n\t"name": "li"\n}')
+  })
+  it('pretty array', () => {
+    const str = '[{name:\'age\'},null,"people",321,true,false]'
+    let out = JsToJson(str)
+    assert.equal(
+      out,
+      '[\n\t{\n\t\t"name": "age"\n\t},\n\tnull,\n\t"people",\n\t321,\n\ttrue,\n\tfalse\n]'
+    )
+  })
+  it('pretty empty  array', () => {
+    const str = '[]'
+    let out = JsToJson(str)
+    assert.equal(out, '[]')
+  })
+  it('pretty escape characters', () => {
+    const str = '[{name:"\\"xiaohuang\\"}]"}]'
+    let out = JsToJson(str)
+    assert.equal(out, '[\n\t{\n\t\t"name": "\\"xiaohuang\\"}]"\n\t}\n]')
+  })
+  it('pretty space', () => {
+    const str = '  [{name:  "\\"xiaohuang\\"}]"}]'
+    let out = JsToJson(str)
+    assert.equal(out, '[\n\t{\n\t\t"name": "\\"xiaohuang\\"}]"\n\t}\n]')
+  })
+  it('delete comment', () => {
+    const str = '{"name":"li"//这是注释\n}'
+    let out = JsToJson(str, { delComment: true })
+    assert.equal(out, '{\n\t"name": "li"\n}')
+  })
+  it('set custom indent', () => {
+    const str = '{"name":"li"\n}'
+    const indent = ' '.repeat(4)
+    let out = JsToJson(str, { indent })
+    assert.equal(out, `{\n${indent}"name": "li"\n}`)
+  })
+})
+
+describe('test getting index of partial valid js value  from start', () => {
+  it('scan array', () => {
+    let origin = '["a",true,null,1.1],'
+    let partialIndex = origin.length - 1
+    let index = JsToJson(origin, { partialIndex: true })
+    assert.equal(index, partialIndex)
+  })
+  it('scan string', () => {
+    let originStrs = [
+      {
+        origin: '"a" ,',
+        expect: '"a"',
+      },
+      {
+        origin: '"abcd",',
+        expect: '"abcd"',
+      },
+      {
+        origin: '"abcd"=true',
+        expect: '"abcd"',
+      },
+      {
+        origin: "'abcd'=true",
+        expect: "'abcd'",
+      },
+      {
+        origin: "'abcd' ,",
+        expect: "'abcd'",
+      },
+      {
+        origin: "'abcd',",
+        expect: "'abcd'",
+      },
+      {
+        origin: '""a=',
+        expect: '""',
+      },
+    ]
+    originStrs.forEach(({ origin, expect }) => {
+      let index = JsToJson(origin, { partialIndex: true })
+      assert.equal(index, expect.length)
     })
-    it('pretty single char empty quotation key object', () => {
-        const str = '{a:1}'
-        let out = JsToJson(str)
-        assert.equal(out, '{\n\t"a": 1\n}')
+  })
+  it('scan object', () => {
+    let originStrs = [
+      {
+        origin: '{"name":123} ,',
+        expect: '{"name":123}',
+      },
+      {
+        origin: '{name:1},',
+        expect: '{name:1}',
+      },
+      {
+        origin: '{"name":"li"}=',
+        expect: '{"name":"li"}',
+      },
+    ]
+    originStrs.forEach(({ origin, expect }) => {
+      let index = JsToJson(origin, { partialIndex: true })
+      assert.equal(index, expect.length)
     })
-    it('pretty empty  object', () => {
-        const str = '{}'
-        let out = JsToJson(str)
-        assert.equal(out, '{}')
-    })
-    it('pretty  object last comma', () => {
-        const str = '{a:1,}'
-        let out = JsToJson(str)
-        assert.equal(out, '{\n\t"a": 1\n}')
-    })
-    it('pretty single quotation object', () => {
-        const str = '{\'name\':\'li\'}'
-        let out = JsToJson(str)
-        assert.equal(out, '{\n\t"name": "li"\n}')
-    })
-    it('pretty array', () => {
-        const str = '[{name:\'age\'},null,"people",321,true,false]'
-        let out = JsToJson(str)
-        assert.equal(
-            out,
-            '[\n\t{\n\t\t"name": "age"\n\t},\n\tnull,\n\t"people",\n\t321,\n\ttrue,\n\tfalse\n]'
-        )
-    })
-    it('pretty empty  array', () => {
-        const str = '[]'
-        let out = JsToJson(str)
-        assert.equal(out, '[]')
-    })
-    it('pretty escape characters', () => {
-        const str = '[{name:"\\"xiaohuang\\"}]"}]'
-        let out = JsToJson(str)
-        assert.equal(out, '[\n\t{\n\t\t"name": "\\"xiaohuang\\"}]"\n\t}\n]')
-    })
-    it('pretty space', () => {
-        const str = '  [{name:  "\\"xiaohuang\\"}]"}]'
-        let out = JsToJson(str)
-        assert.equal(out, '[\n\t{\n\t\t"name": "\\"xiaohuang\\"}]"\n\t}\n]')
-    })
-    it('delete comment', () => {
-        const str = '{"name":"li"//这是注释\n}'
-        let out = JsToJson(str, { delComment: true })
-        assert.equal(out, '{\n\t"name": "li"\n}')
-    })
-    it('set custom indent', () => {
-        const str = '{"name":"li"\n}'
-        const indent = ' '.repeat(4)
-        let out = JsToJson(str, { indent })
-        assert.equal(out, `{\n${indent}"name": "li"\n}`)
-    })
+  })
 })
